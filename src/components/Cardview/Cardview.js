7d78cf9f1f6d4caf89e56f655a9f9d947f406b5b
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2020-01-21 13:35:07 +0545

    Display wrcians in a card view

---
+++ src/components/Cardview/Cardview.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import OneCard from '../OneCard/OneCard';
+const dumbasData = require('../../assets/data/dumbasData.json')
+
+
+const useStyles = makeStyles({
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
+function CardView() {
+    console.log(dumbasData);
+    return dumbasData.map((item, index) => {
+        return (
+            <OneCard key={index} personObject={item} />
+        );
+    })
+
+}
+
+export default CardView;
